e0a1df097db70f830ae22934d8b1d90e2fd7ba13
stjer <58432049+stjer@users.noreply.github.com>
2024-06-20 15:18:42 +0900

    Update 기보캡쳐기.py

--- 기보캡쳐기.py
+++ 기보캡쳐기.py
@@ -1,35 +1,25 @@
 import os
-import sys
 import time
 import mss
 import mss.tools
 import pyautogui
-import natsort
-import shutil
 
 from pynput import mouse
 from pynput.keyboard import Key, Controller
 from PIL import Image
 
-#from PySide6.QtCore import QSize, Qt
-#from PySide6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMainWindow, QVBoxLayout, \
-#    QHBoxLayout, QSlider
 
-def tryint(maxn = 100,s=""):
-    try : 
+def tryint(maxn=100, s=""):
+    try:
         a = int(input(s))
-        if (0<=a<=maxn):
-            return a
-        elif a== -1:
+        if 0 <= a <= maxn or a == -1:
             return a
         else:
             print("메뉴에 있는 번호를 선택해 주세요.")
-            a = tryint()
-            return a
-    except: 
+            return tryint(maxn, s)
+    except ValueError:
         print("숫자를 입력해 주세요.")
-        a = tryint()
-        return a
+        return tryint(maxn, s)
 
 def xy():
     def on_click(x, y, button, pressed):
@@ -43,7 +33,6 @@ def xy():
         if not pressed:
             return False
         
-
     with mouse.Listener(on_click=on_click) as listener:
         listener.join()
 
@@ -53,28 +42,33 @@ y1 = 0
 def main():
 
     num = 1
-    posX1 = 684
-    posY1 = 98
-    posX2 = 1237
-    posY2 = 650
-    posX3 = 1109
-    posY3 = 679
-    page = 201
+    left_up = (684, 98)
+    right_down = (1237, 650)
+    n_ext = (1109, 679)
+    gozero = (714, 683)
+    goback = (716, 66)
+    page = 1
     speed = 0.45
     region = {}
     print("default : 0, change : 1")
     changxy = tryint(1,"좌표 변경 : ")
     if changxy == 1:
-        #print("")
+        print("좌상단 : ", end = "")
         xy()#left_up
-        posX1, posY1 = x1,y1
+        left_up = (x1,y1)
+        print("우하단 : ", end = "")
         xy()#right_down
-        posX2, posY2 = x1,y1
+        right_down = (x1,y1)
+        print("다음 : ", end = "")
         xy()#next
-        posX3, posY3 = x1,y1
-    changp = tryint(1,"페이지 수 변경 : ")
-    if changp == 1:
-        page = tryint(201, "페이지 수 : ")
+        n_ext = (x1,y1)
+        print("맨앞으로 : ", end = "")
+        xy()#gozero
+        gozero = (x1, y1)
+        print("뒤로가기 : ", end = "")
+        xy()#goback
+        goback = (x1, y1)
+    page = tryint(201, "페이지 수 : ")
     giboname = input("생성할 기보의 이름을 입력하세요.")
     
     #pos_x, pos_y = pyautogui.position()
@@ -83,23 +77,15 @@ def main():
         os.mkdir(os.path.join(title))
 
     # The screen part to capture
-    region = {'top': posY1, 'left': posX1, 'width': posX2 - posX1,
-              'height': posY2 - posY1}
+    region = {'top': left_up[1], 'left': left_up[0], 'width': right_down[0] - left_up[0],
+        'height': right_down[1] - left_up[1]}
 
     m = mouse.Controller()
     mouse_left = mouse.Button.left
-    kb_control = Controller()
 
     try:
-        # 화면 전환 위해 한번 클릭
-        #time.sleep(2)
-        #m.position = (posX1, posY1)
-
-        #time.sleep(2)
-        #m.click(mouse_left)
-        #time.sleep(2)
-        #m.position = (pos_x, pos_y)
-
+        m.position = gozero
+        m.click(mouse_left)
         # 파일 저장
         while num <= page:
 
@@ -113,20 +99,16 @@ def main():
                 mss.tools.to_png(img.rgb, img.size, output=f'{title}/img_{str(num).zfill(4)}.png' )
 
             # 페이지 넘기기
-            m.position = (posX3, posY3)
-
-            #time.sleep(1)
+            m.position = n_ext
             m.click(mouse_left)
-            #time.sleep(1)
+            
 
             num += 1
 
         print("캡쳐 완료!")
+        m.position = goback
+        m.click(mouse_left)
         
     except Exception as e:
-        print('예외 발생. ', e)
-        stat.setText('오류 발생. 종료 후 다시 시도해주세요.')
-
-        
-main()
+        print('오류 발생. ', e)
 
